aa1bc16d7cf45349961ae571b591722ad325417a
vatsal prajapati <vatsprajapati20@gmail.com>
2023-12-29 11:57:09 +0530

    Node Modules

--- hello.js
+++ hello.js
@@ -1,1 +1,5 @@
-console.log("hello world")
+const {add,sub} = require('./math'); 
+
+
+console.log("math value is: ", add(2,3) );
+console.log("math value is: ", sub(5,2) );
---
+++ math.js
@@ -0,0 +1,18 @@
+// Default exports
+// 1).
+// function add(a,b) {
+//     return a+ b;
+// }
+
+// function sub(a,b) {
+//     return a - b;
+// }
+
+// module.exports = {
+//     add,
+//     sub
+// }
+
+// 2).
+exports.add = (a,b) => a+b ;
+exports.sub = (a,b) => a-b ;
